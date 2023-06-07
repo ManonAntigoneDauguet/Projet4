85ce5e667fbeab80f8a7e12bd4f7e439f396a0f6
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-06-07 11:13:15 +0200

    feat: le formulaire doit être valide pour être envoyé.
    Les champs Prénom, Nom, email, nombre de concours, lieu de concours(radio button) doivent être remplis ET valides.
    La case de conditions générales doit être cochée.
    Les données du formulaire sont conservées lorsqu'il ne passe pas la validation.

--- GameOn-website-FR-master/modal.js
+++ GameOn-website-FR-master/modal.js
@@ -12,6 +12,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
+const locationInput = document.querySelectorAll('input[name="location"]');
+const conditionsInput = document.querySelector("#checkbox1");
+const form = document.querySelector('form[name="reserve"]');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,4 +30,31 @@ closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+// say if a location is selected
+function isLocationSelected() {
+  for (let i = 0; i < locationInput.length; i++) {
+    if (locationInput[i].checked) {
+      return true;
+    }
+  }
+  alert("Veillez choisir un tournois");
+  return false;
+}
+
+// say if conditions-checkbox is checked
+function isConditionAccepted() {
+  if (conditionsInput.checked) {
+    return true;
+  } 
+  alert("Veillez accepter les conditions d'utilisation"); 
+  return false;   
+}
+
+// validation modal form
+function validate(event) {
+  if (!isLocationSelected() || !isConditionAccepted()) {
+    event.preventDefault();
+  }
 }
