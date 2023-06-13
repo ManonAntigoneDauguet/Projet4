551999c5cbd75298846ed2f320b026ca9f751f67
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-06-13 10:45:10 +0200

    feat: l'acceptabilité du formulaire est entièrement géré en JavaScript

--- GameOn-website-FR-master/modal.js
+++ GameOn-website-FR-master/modal.js
@@ -12,10 +12,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const formDataInputs = document.querySelectorAll(".formData input");
 const closeBtn = document.querySelectorAll(".close");
 const locationInput = document.querySelectorAll('input[name="location"]');
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
+const conditionsInput = document.querySelector("#checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -43,52 +43,85 @@ function isLocationSelected() {
   return false;
 }
 
+// say if conditions-checkbox is checked
+function isConditionAccepted() {
+  if (conditionsInput.checked) {
+    return true;
+  } 
+  return false;   
+}
+
+// say if inputs are valide
+function isInputValid() {
+  for (let i = 0; i < formDataInputs.length; i++) {
+    let input = formDataInputs[i];
+    let inputName = input.name;
+    let inputValue = input.value; 
+    if (inputValue === "") {
+      return false;
+    } 
+  }
+  return true;
+}
+
 // validation message
-async function validate(event) {
+function validate(event) {
   event.preventDefault();
-  alert("Merci pour votre inscription !");
-  closeModal();
+  if (isInputValid() && isConditionAccepted() && isLocationSelected()) {
+    alert("Merci pour votre inscription !");
+    closeModal();
+  } else {
+    alert("Le formulaire n'est pas valide");
+  }
 }
 
 // errors messages on input modal
-function isInputValid(inputName) {
-  const input = document.querySelector(`#${inputName}`);
-  let inputRecommendation = "";
-    switch (inputName) {
-      case 'first':
-        inputRecommendation = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-        break;
+// function letErrorMessages() {
+//   for (let i = 0; i < formDataInputs.length; i++) {
+//     let input = formDataInputs[i];
+//     let inputName = input.name;
+//     let inputValue = input.value; 
+//     let inputRecommendation = "";
+//     console.log(inputValue);
+//       switch (inputName) {
+//         case 'first':
+//           if (inputValue === "") {
+//             inputRecommendation = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';            
+//           } 
+//           break;
 
-      case 'last':
-        inputRecommendation = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
-        break;
+//         case 'last':
+//           inputRecommendation = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
+//           break;
 
-      case 'email':
-        inputRecommendation =  'Veillez rentrer un mail valide.';
-        break;
+//         case 'email':
+//           inputRecommendation =  'Veillez rentrer un mail valide.';
+//           break;
 
-      case 'birthdate':
-        inputRecommendation = 'Veillez entrer votre date de naissance.';
-        break;
+//         case 'birthdate':
+//           inputRecommendation = 'Veillez entrer votre date de naissance.';
+//           break;
 
-      case 'quantity':
-        inputRecommendation = 'Veillez répondre à la question.';
-        break;
+//         case 'quantity':
+//           inputRecommendation = 'Veillez répondre à la question.';
+//           break;
 
-      case 'checkbox1':
-        inputRecommendation = 'Veillez accepter les termes et conditions d\'utilisation';
-        break;
-      
-      case 'location1':
-        if (isLocationSelected()) {
-          document.querySelector("#location1").removeAttribute("required");
-        } else {
-          inputRecommendation = "Veillez choisir un tournois";          
-        }
-        break;
+//         case 'checkbox1':
+//           inputRecommendation = 'Veillez accepter les termes et conditions d\'utilisation';
+//           break;
+        
+//         case 'location':
+//           if (isLocationSelected()) {
+//             document.querySelector("#location1").removeAttribute("required");
+//           } else {
+//             inputRecommendation = "Veillez choisir un tournois";          
+//           }
+//           break;
 
-      default:
-        inputRecommendation = 'Veillez répondre à la question.';
-    }
-  input.setCustomValidity(inputRecommendation);
-}
+//         default:
+//           inputRecommendation = 'Veillez répondre à la question.';
+//       }
+//     input.setCustomValidity(inputRecommendation);
+//   }
+// }  
+
